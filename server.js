1776b6ee94fde4e883178ba7be7db96e471692af
sivanathan vithurshan <vithurshans.22@uom.lk>
2024-12-26 13:12:59 +0530

    progress added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173','http://192.168.58.41:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
